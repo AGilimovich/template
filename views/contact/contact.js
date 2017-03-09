814787200fe22491c10f41b133847e5a25d07c0c
AGilimovich <a.gilimovich1990@gmail.com>
2017-03-09 21:15:30 +0300

    --

--- views/contact/contact.js
+++ views/contact/contact.js
@@ -16,7 +16,6 @@ btnAddPhone.onclick = function (ev) {
 }
 
 btnUndoPhone.onclick = function () {
-    //phonePopup.style.display = "none";
     phonePopup.className = "popup";
 }
 
@@ -31,14 +30,21 @@ var btnUndoAttach = document.getElementById("btn-undo-attach");
 
 var btnSaveAttach = document.getElementById("btn-save-attach");
 
-btnAddAttach.onclick = function (ev) {
-    ev.preventDefault();
-
-    //phonePopup.style.display = "block";
+btnAddAttach.onclick = function () {
     attachPopup.className += " show";
 }
 
 btnUndoAttach.onclick = function () {
-    //phonePopup.style.display = "none";
     attachPopup.className = "popup";
+}
+
+
+var photo = document.getElementById('photo');
+var photoPopup = document.getElementById('photo-popup');
+var btnUndoPhoto = document.getElementById("btn-undo-photo");
+photo.onclick = function () {
+    photoPopup.className += " show";
+}
+btnUndoPhoto.onclick = function () {
+    photoPopup.className = "popup";
 }
